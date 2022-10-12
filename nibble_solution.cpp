4d1df4fa892984a800a031c5ee0bcd27668f6998
Casey <44584719+caseyhild@users.noreply.github.com>
2022-10-11 22:59:42 -0700

    Update nibble_solution.cpp

--- nibble_solution.cpp
+++ nibble_solution.cpp
@@ -8,18 +8,9 @@ extern "C" uint64_t nibble_search(uint8_t query, const std::vector<uint16_t> & t
     uint64_t count = 0;
 
     for(auto & i: targets) {
-        count += (((i) & (0xf)) == query);
-        count += (((i >> 1) & (0xf)) == query);
-        count += (((i >> 2) & (0xf)) == query);
-        count += (((i >> 3) & (0xf)) == query);
-        count += (((i >> 4) & (0xf)) == query);
-        count += (((i >> 5) & (0xf)) == query);
-        count += (((i >> 6) & (0xf)) == query);
-        count += (((i >> 7) & (0xf)) == query);
-        count += (((i >> 8) & (0xf)) == query);
-        count += (((i >> 9) & (0xf)) == query);
-        count += (((i >> 10) & (0xf)) == query);
-        count += (((i >> 11) & (0xf)) == query);
+        count += (((i) & (0xf)) == query) + (((i >> 1) & (0xf)) == query) + (((i >> 2) & (0xf)) == query) + (((i >> 3) & (0xf)) == query)
+            + (((i >> 4) & (0xf)) == query) + (((i >> 5) & (0xf)) == query) + (((i >> 6) & (0xf)) == query) + (((i >> 7) & (0xf)) == query)
+            + (((i >> 8) & (0xf)) == query) + (((i >> 9) & (0xf)) == query) + (((i >> 10) & (0xf)) == query) + (((i >> 11) & (0xf)) == query);
     }
     return count;
 }
